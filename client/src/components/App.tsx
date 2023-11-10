6d87967c569b4a3da5135c399d06027ae232cc4f
hasan-na <hasannassar39@gmail.com>
2023-11-09 17:02:03 -0700

    overwriting the app file

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,18 +1,61 @@
-import { useState } from 'react'
+import Register from './Register';
+import Main from './Main';
+import { useState } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [userID, setUserID] = useState(-1);
+  const [name, setName] = useState("");
+
+  const { sendJsonMessage, readyState } = useWebSocket(import.meta.env.VITE_WS_URL, {
+    share: true,
+    onOpen: () => {
+      console.log("Websocket connection established.");
+    },
+    onMessage: m => {
+      const event = JSON.parse(m.data);
+      if (event.type === "register") {
+        console.log(event);
+        setUserID(event.id);
+      }
+    },
+    filter: m => {
+      const event = JSON.parse(m.data);
+      return event.type === "register";
+    },
+    shouldReconnect: () => true,
+  });
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
+  const handleClickRegister = (n: string) => {
+    sendJsonMessage({name: n});
+    setName(n);
+  }
 
   return (
-    <>
-      <p className="text-red-600 bg-blue-500">
-        Hello world!
-      </p>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </>
-  )
+    <div className="ml-2 mt-4">
+    {
+      userID < 0 ? (
+        <Register
+          handleClickRegister={n => handleClickRegister(n)}
+          connectionStatus={connectionStatus}
+          duplicate={userID === -2}
+        />
+      ) : (
+        <Main
+          name={name}
+        />
+      )
+    }
+    </div>
+  );
 }
 
-export default App
+export default App;
