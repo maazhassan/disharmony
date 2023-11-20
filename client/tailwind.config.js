077bdac7c68db70bc29760168fe144e8443f4dcf
Dawood Afzaal <dawoodafzaal13@gmail.com>
2023-11-20 13:51:16 -0700

    added css var colors to tailwind css config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-color": "var(--bg-color: #4C4254)",
+        "app-orang": "var(--app-orange: #ED7B82)",
+        "modal-color": "var(--modal-color: #635971)",
+        "ban-color": "var(--ban-btn: #C73F47)",
+        "app-pink": "var(--pink: #ed7b82)",
+      },
+    },
   },
   plugins: [],
 }
