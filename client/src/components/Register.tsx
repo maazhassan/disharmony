98c0daa097307539f2041bf7acb6969c9a05cbea
Maaz Hassan <h.maaz809@hotmail.com>
2023-11-27 03:29:06 -0700

    Migrate to tailwind, restructure

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,65 +1,48 @@
 import React, { useState } from 'react';
-import '../login.css'
 
 type RegisterProps = {
-  handleClickRegister: (n: string) => void,
+  handleClickLogin: (u: string, p: string) => void,
   connectionStatus: string,
-  duplicate: boolean
 }
 
 
-const Register = ({ handleClickRegister, connectionStatus, duplicate }: RegisterProps) => {
+const Register = ({ handleClickLogin, connectionStatus }: RegisterProps) => {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
 
-  const handlePasswordChange = (event : React.ChangeEvent<HTMLInputElement>) => {
-    console.log(event.target.value);
-    setPassword(event.target.value);
-  }
-  
-  
-
   return (
-    <div className='box'>
-      <div className='login-container'>
-        <div className='login-modal'>
-          <h2>
-            Login
-          </h2>
+    <div className="flex justify-center">
+      <div className="flex flex-col mt-32 items-center h-[30rem] w-[25rem] bg-modal-color rounded-lg">
+        <h2 className="text-white text-2xl mt-10">
+          Login
+        </h2>
+        <input
+            className="mt-10 indent-3 rounded w-72 h-10 border-none focus:outline-none"
+            type='text'
+            placeholder='Username...'
+            onChange={e => setName(e.target.value)}
+          />
           <input
-              className="border border-black rounded"
-              type='text'
-              placeholder='Username...'
-              onChange={e => setName(e.target.value)}
-            />
-            <input
-              value={password}
-              type="password"
-              placeholder="Password"
-              onChange={e => handlePasswordChange(e)}
-            >
-            </input>
-          <button 
-            className="border border-black rounded bg-green-400 ml-1 disabled:bg-red-400"
-            onClick={() => handleClickRegister(name)}
-            disabled={connectionStatus !== "Open" || name.length === 0}
+            className="mt-10 indent-3 rounded w-72 h-10 border-none focus:outline-none"
+            type="password"
+            placeholder="Password..."
+            onChange={e => setPassword(e.target.value)}
           >
-            Login
-          </button>
-          <div className='make-acc-link'>
-            Don't have an account? <span>Click here</span>
-          </div>
+          </input>
+        <button 
+          className="py-2 px-8 mt-12 bg-app-pink text-white border-none rounded font-semibold hover:cursor-pointer"
+          onClick={() => handleClickLogin(name, password)}
+          disabled={connectionStatus !== "Open" || name.length === 0}
+        >
+          Login
+        </button>
+        <div className="text-white mt-24">
+          Don't have an account?&nbsp;
+          <span className="text-app-pink underline hover:cursor-pointer hover:text-app-pink">
+            Click here!
+          </span>
         </div>
       </div>
-
-      {duplicate &&
-        <span className="ml-2 text-red-600 font-bold">
-          A client with this name is already connected. Try again.
-        </span>
-      }
-      <p className={`${connectionStatus === "Open" ? "text-green-600" : "text-red-600"}`}>
-        {connectionStatus === "Open" ? "Connected." : "Connecting..."}
-      </p>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,24 +6,12 @@ export default {
   ],
   theme: {
     extend: {
-      height: {
-        '45rem': '45rem',
-        'inputh': '2.5rem',
-      },
-      width: {
-        '33.5rem': '33.5rem',
-        'inputm': '2.75rem',
-        'inputw': '17.5rem',
-      },
-      spacing: {
-        't-indent': '0.74rem',
-      },
       colors: {
-        "bg-color": "var(--bg-color: #4C4254)",
-        "app-orang": "var(--app-orange: #ED7B82)",
-        "modal-color": "var(--modal-color: #635971)",
-        "ban-color": "var(--ban-btn: #C73F47)",
-        "app-pink": "var(--pink: #ed7b82)",
+        "bg-color": "#4C4254",
+        "app-orange": "#ED7B82",
+        "modal-color": "#635971",
+        "ban-color": "#C73F47",
+        "app-pink": "#ED7B82",
       },
     },
   },
