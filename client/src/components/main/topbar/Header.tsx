e6d65343b0de4ea273073b941fb9e3d15b7dd942
Dawood Afzaal <dawoodafzaal13@gmail.com>
2023-12-04 12:05:44 -0700

    adjusted block users modal to clean UI design

--- client/src/components/main/topbar/Header.tsx
+++ client/src/components/main/topbar/Header.tsx
@@ -115,18 +115,26 @@ const Header = ({ friendRequests, blocked, onRespond, onUnblock, onFriendReq, on
       <ReactModal
         isOpen={bmodalIsOpen}
         onRequestClose={closeBModal}
-        className={"fixed w-80 h-[30rem] bg-bg-color rounded-md outline-none top-10 right-28"}
+        className={"fixed w-80 h-[27rem] bg-bg-color rounded-md outline-none top-10 right-28"}
         overlayClassName={"fixed top-0 left-0 right-0 bottom-0 bg-white/10"}
       >
         <ul className="h-[79%] overflow-auto">
-          <li className="flex flex-row justify-center text-white text-xl font-medium mt-2">Blocked Users</li>
+          <span className="flex flex-row justify-center">
+            <div></div>
+            <li className="flex flex-row justify-center text-white text-xl font-medium mt-2">Blocked Users</li>
+            <FontAwesomeIcon 
+              icon={faSquareXmark}
+              className="h-8 w-8 pt-2 absolute right-2.5 hover:cursor-pointer"
+              onClick={closeBModal}
+            />
+          </span>
           {blocked.map((user, idx) =>
             <li key={idx} className="flex flex-row justify-center">
               <div className="relative bg-modal-color w-[90%] mt-2 rounded-md py-2">
                 <span className="text-white text-xl font-medium ml-4">{user}</span>
                 <FontAwesomeIcon
                   icon={faSquareXmark}
-                  className="h-8 w-8 absolute right-4 text-app-pink hover:cursor-pointer" 
+                  className="h-8 w-8 absolute right-5 text-app-pink hover:cursor-pointer" 
                   onClick={() => onUnblock(user)}
                 />
               </div>
@@ -134,18 +142,12 @@ const Header = ({ friendRequests, blocked, onRespond, onUnblock, onFriendReq, on
           )}
         </ul>
         <input
-          className="absolute bottom-14 right-0 left-0 w-fit mx-auto bg-text-input-bg focus:outline-none rounded text-white py-1 px-1 placeholder:text-gray-200"
+          className="absolute bottom-9 right-0 left-0 w-fit mx-auto bg-text-input-bg focus:outline-none rounded text-white py-1 px-1 placeholder:text-gray-200"
           placeholder="Search..."
           onKeyDown={e => onKeyDown(e, "friend")}
           onChange={e => onChange(e)}
           value={inputText}
         />
-        <button
-          onClick={closeBModal}
-          className="absolute bottom-3 right-0 left-0 w-fit mx-auto py-1 px-8 bg-app-pink text-white border-none rounded font-semibold hover:cursor-pointer"
-        >
-          Close
-        </button>
       </ReactModal>
     </div>
   );
